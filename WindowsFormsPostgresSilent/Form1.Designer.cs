31b8f80e2c6c3f8801c997bc13e328a17fff96dc
Ashutosh Tayade <ashutoshtayade3@gmail.com>
2022-06-01 14:01:07 +0530

    Add project files.

---
+++ WindowsFormsPostgresSilent/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WindowsFormsPostgresSilent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Postgres Installation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Uninstall";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Progress";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(374, 277);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ WindowsFormsPostgresSilent/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPostgresSilent
+{
+    public partial class Form1 : Form
+    {
+
+        public int currentCounter = 1;
+        Process procPotgresInstall = new Process();
+
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+
+                Task.Run(() => {
+
+                    InstallPostgres();
+
+                });
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void InstallPostgres()
+        {
+            string filePath = @"E:\Postgres\SilentPostgresInstall.bat";
+            ProcessStartInfo processPostgres = new ProcessStartInfo();
+
+            processPostgres.CreateNoWindow = true; //This hides the dos-style black window that the command prompt usually shows
+            processPostgres.FileName = @"cmd.exe";
+            processPostgres.WindowStyle = ProcessWindowStyle.Hidden;
+            //psi.UseShellExecute = false;
+
+            processPostgres.Verb = "runas"; //This is what actually runs the command as administrator
+            processPostgres.Arguments = "/C " + filePath;
+            try
+            {
+                var process = new Process();
+                process.StartInfo = processPostgres;
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                //If you are here the user clicked decline to grant admin privileges (or he's not administrator)
+            }
+        }
+
+
+        //private void UninstallPostgres()
+        //{
+        //    ProcessStartInfo processPostgres = new ProcessStartInfo();
+
+        //    processPostgres.CreateNoWindow = true; //This hides the dos-style black window that the command prompt usually shows
+           
+        //    processPostgres.WindowStyle = ProcessWindowStyle.Hidden;
+        //    //psi.UseShellExecute = false;
+        //    processPostgres.WorkingDirectory = @"C:\Program Files\PostgreSQL\14";
+        //    processPostgres.FileName = @"C:\Program Files\PostgreSQL\14\cmd.exe";
+        //    processPostgres.Verb = "runas"; //This is what actually runs the command as administrator
+        //    processPostgres.Arguments = "/C " + filePath + "uninstall - postgresql.exe--mode unattended";
+        //    try
+        //    {
+        //        var process = new Process();
+        //        process.StartInfo = processPostgres;
+        //        process.Start();
+        //        process.WaitForExit();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //If you are here the user clicked decline to grant admin privileges (or he's not administrator)
+        //    }
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker2.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+
+                Task.Run(() => {
+
+                   // UninstallPostgres();
+
+                });
+
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            bool IsPostgresInstall = false;
+
+            do
+            {
+                System.ServiceProcess.ServiceController ctl = System.ServiceProcess.ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "postgreSQL");
+
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    if (ctl != null)
+                    {
+                        if (ctl.Status == System.ServiceProcess.ServiceControllerStatus.Running)
+                        {
+
+                            IsPostgresInstall = true;
+                            currentCounter = progressBar1.Maximum;
+                        }
+                    }
+
+                    worker.ReportProgress(currentCounter, IsPostgresInstall);
+                }
+
+
+                System.Threading.Thread.Sleep(1000);
+                currentCounter++;
+            } while (!IsPostgresInstall);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage < progressBar1.Maximum && Convert.ToBoolean(e.UserState) == false) // INSTALATION ELASPED TIME GOING AND INSTALION NOT FINISHED
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }
+            else if (e.ProgressPercentage == progressBar1.Maximum && Convert.ToBoolean(e.UserState) == true) // INSTALATION ELASPED TIME FINISHED AND INSTALION FINISHED
+            {
+                progressBar1.Value = progressBar1.Maximum;
+            }
+            else if (e.ProgressPercentage == progressBar1.Maximum && Convert.ToBoolean(e.UserState) == false)
+            {
+
+                //procPotgresInstall.Kill();
+                // procPotgresInstall.Close();
+                // procPotgresInstall.Dispose();
+                //label1.Text = (e.ProgressPercentage.ToString() + "%");
+
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                label1.Text = "Canceled!";
+            }
+            else if (e.Error != null)
+            {
+                label1.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                label1.Text = "Done!";
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            bool IsPostgresUninstall = false;
+
+            do
+            {
+                System.ServiceProcess.ServiceController ctl = System.ServiceProcess.ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "postgreSQL");
+
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    if (ctl != null)
+                    {
+                        if (ctl.Status == System.ServiceProcess.ServiceControllerStatus.Running)
+                        {
+                            worker.ReportProgress(currentCounter, IsPostgresUninstall);
+                            IsPostgresUninstall = false;
+                            
+                        }
+                    }
+                    IsPostgresUninstall = true;
+                    currentCounter = progressBar1.Maximum;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+                currentCounter++;
+            } while (!IsPostgresUninstall);
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage < progressBar1.Maximum && Convert.ToBoolean(e.UserState) == false) // INSTALATION ELASPED TIME GOING AND INSTALION NOT FINISHED
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }
+            else if (e.ProgressPercentage == progressBar1.Maximum && Convert.ToBoolean(e.UserState) == true) // INSTALATION ELASPED TIME FINISHED AND INSTALION FINISHED
+            {
+                progressBar1.Value = progressBar1.Maximum;
+            }
+            else if (e.ProgressPercentage == progressBar1.Maximum && Convert.ToBoolean(e.UserState) == false)
+            {
+
+                //procPotgresInstall.Kill();
+                // procPotgresInstall.Close();
+                // procPotgresInstall.Dispose();
+                //label1.Text = (e.ProgressPercentage.ToString() + "%");
+
+            }
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+}
